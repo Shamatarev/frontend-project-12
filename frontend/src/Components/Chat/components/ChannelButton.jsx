fbd99840d1c1a22c3500ab90f997248ec98f8d91
Shamatarev <sh.a.v@list.ru>
2023-09-08 23:52:33 +0300

    1.6

---
+++ frontend/src/Components/Chat/components/ChannelButton.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Button, Dropdown, ButtonGroup } from 'react-bootstrap';
+import cn from 'classnames';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import ChannelName from '../../common/ChannelName.jsx';
+import ChannelModalDel from './ModalChannelDel.jsx';
+import ChannelModalUpdate from './ModalChannelUpdate.jsx';
+import { actions as modalActions, selectors as modalSelectors } from '../../../slices/modal.js';
+
+const ChannelButton = ({
+  channel, isActive, onClick, removable,
+}) => {
+  const { t } = useTranslation();
+  const { name, id } = channel;
+  const dispatch = useDispatch();
+  const isOpened = useSelector(modalSelectors.isModalOpened);
+  const modalType = useSelector(modalSelectors.getModalType);
+
+  const classNameMainButton = cn('w-100 rounded-0 text-start btn', {
+    'btn-primary': isActive,
+    'btn-light': !isActive, // Добавляем класс btn-light для неактивных кнопок
+  });
+
+  const classNameDropdown = cn('border-0 rounded-0', {
+    'btn-primary': isActive,
+    'btn-light': !isActive, // Добавляем класс btn-light для неактивных кнопок
+  });
+
+  const handleButtonClickRemoveChannel = () => {
+    if (removable) {
+      console.log('Delete button clicked');
+      dispatch(modalActions.open({ type: 'delete' }));
+    }
+  };
+
+  const handleButtonUpdateClickChannel = () => {
+    if (removable) {
+      console.log('Rename button clicked');
+      dispatch(modalActions.open({ type: 'rename' }));
+    }
+  };
+
+  const handleCloseModal = () => {
+    dispatch(modalActions.close());
+  };
+
+  return (
+    <Dropdown as={ButtonGroup} className="d-flex">
+      <Button type="button" className={classNameMainButton} onClick={() => onClick(channel.id)}>
+        <ChannelName name={name} />
+      </Button>
+      {removable && (
+        <div className="d-flex">
+          <Dropdown.Toggle split placeholder={t('channelControl')} className={classNameDropdown}>
+            <span className="visually-hidden">{t('channelControl')}</span>
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={handleButtonClickRemoveChannel}>{t('remove')}</Dropdown.Item>
+            <Dropdown.Item onClick={handleButtonUpdateClickChannel}>{t('rename')}</Dropdown.Item>
+          </Dropdown.Menu>
+        </div>
+      )}
+
+      {isOpened && modalType === 'delete' && (
+        <ChannelModalDel show={isOpened} id={id} handleClose={handleCloseModal} />
+      )}
+
+      {isOpened && modalType === 'rename' && (
+        <ChannelModalUpdate show={isOpened} id={id} handleClose={handleCloseModal} />
+      )}
+    </Dropdown>
+  );
+};
+
+export default ChannelButton;
--- frontend/src/Components/Chat/components/ChannelsBox.jsx
+++ frontend/src/Components/Chat/components/ChannelsBox.jsx
@@ -5,8 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { selectors, changeChannelId } from '../../../slices/channels';
 import { ChannelModalAdd } from './ModalСhannelAdd.jsx';
-import SimpleButton from './ButtonSimpleChannel.jsx';
-import DropButton from './ButtonDropdownChannel.jsx';
+import ChannelButton from './ChannelButton.jsx';
 
 const ChannelsBox = () => {
   const { t } = useTranslation();
@@ -15,11 +14,10 @@ const ChannelsBox = () => {
   const dispatch = useDispatch();
 
   const handleButtonClick = (channelId) => {
-    dispatch(changeChannelId(channelId)); // Отправляем действие для обновления currentChannelId
+    dispatch(changeChannelId(channelId));
   };
 
   return (
-
     <Formik>
       <Form className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
         <Form className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
@@ -28,32 +26,18 @@ const ChannelsBox = () => {
         </Form>
 
         <ul id="channels-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
-
           {channels.map((channel) => (
-
             <li key={channel.id} className="nav-item w-100">
-              { channel.removable === true
-                ? (
-                  <DropButton
-                    channel={channel}
-                    isActive={currentChannelId === channel.id}
-                    onClick={() => handleButtonClick(channel.id)}
-                  />
-                )
-                : (
-                  <SimpleButton
-                    channel={channel}
-                    isActive={currentChannelId === channel.id}
-                    onClick={() => handleButtonClick(channel.id)}
-                  />
-                )}
+              <ChannelButton
+                channel={channel}
+                isActive={currentChannelId === channel.id}
+                onClick={() => handleButtonClick(channel.id)}
+              />
             </li>
           ))}
         </ul>
-
       </Form>
     </Formik>
-
   );
 };
 
