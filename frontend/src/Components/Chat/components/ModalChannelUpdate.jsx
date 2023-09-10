7ba75cab3e81b5eb5be3ca0d393e8e9ad566f17f
Shamatarev <sh.a.v@list.ru>
2023-09-10 23:01:47 +0300

    fix revchannel

--- frontend/src/Components/Chat/components/ModalChannelUpdate.jsx
+++ frontend/src/Components/Chat/components/ModalChannelUpdate.jsx
@@ -73,6 +73,7 @@ const ChannelModalUpdate = ({ show, handleClose, id }) => {
               name="name"
               value={channelName}
               className="mb-2 form-control"
+              placeholder="Имя канала"
               onChange={(e) => {
                 setChannelName(e.target.value);
                 setIsInvalid(false); // Сбрасываем стили при изменении поля ввода
